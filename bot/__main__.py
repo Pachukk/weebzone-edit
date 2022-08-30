383341b6c7c1a6a9ffce951f057d8a70063a056a
Pachukk <88023964+Pachukk@users.noreply.github.com>
2022-08-31 01:34:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -259,7 +259,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         else:
             sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        text = f"Not Authorized user, deploy your own mirror bot"
+        text = f"Not Authorized user"
         if PICS:
             sendPhoto(text, context.bot, update.message, random.choice(PICS), reply_markup)
         else:
